17c75ef9ba22a5e345e8037a6e2be8a0bdc85795
Daniel Luevano <daniel_luevanolr@hotmail.com>
2024-10-18 17:24:44 -0600

    edition next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     trailingSlash: true, // Para asegurar que las rutas funcionen correctamente
+    images: {
+      domains: ['raw.githubusercontent.com'], // Agrega aquí el dominio de las imágenes que usas
+    },
   };
   
   export default nextConfig;

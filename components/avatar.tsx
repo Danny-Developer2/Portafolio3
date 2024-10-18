067bb773d29d1349370a4b26a972f07b3b05fe98
Daniel Luevano <daniel_luevanolr@hotmail.com>
2024-10-17 21:18:51 -0600

    Compilación exitosa y lista para desplegar 4

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,7 +1,7 @@
 
 "use client"
 
-import avatar from "../public/home-4.png";
+
 import Image from "next/image";
 
 import { MotionTransition } from "./transition-component";
@@ -9,7 +9,7 @@ import { MotionTransition } from "./transition-component";
 export function Avatar() {
     return (
         <MotionTransition position="bottom" className="bottom-0 right-0 hidden md:inline-block md:absolute ">
-            <Image src={avatar} width="400" height="400" className="w-full h-full " alt="Particles " />
+            <Image src="/home-4.png" width="400" height="400" className="w-full h-full " alt="Particles " />
         </MotionTransition>
     )
 }

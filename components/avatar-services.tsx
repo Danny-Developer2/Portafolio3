585619716b871828f10359ee868c11170da42ad9
Daniel Luevano <daniel_luevanolr@hotmail.com>
2024-10-17 21:11:31 -0600

    Compilación exitosa y lista para desplegar 3

--- components/avatar-services.tsx
+++ components/avatar-services.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import { MotionTransition } from "./transition-component";
-import Service from '../public/services.png'
+import service from '../public/services.png'
 
 const AvatarServices = () => {
     return (
         <MotionTransition position='right' className="bottom-0 left-0 hidden md:inline-block md:absolute">
-            <Image src={Service} width="400" height="400" className="w-[350px] h-full " alt="Particles " />
+            <Image src={service} width="400" height="400" className="w-[350px] h-full " alt="Particles " />
         </MotionTransition>
     );
 }

1f78302e59dc814c47b2bcc62a202725bcb473b8
Daniel Luevano <daniel_luevanolr@hotmail.com>
2024-10-17 23:55:20 -0600

    images update

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -3,13 +3,13 @@
 
 
 import Image from "next/image";
-
+import home from "../public/home-4.png"
 import { MotionTransition } from "./transition-component";
 
 export function Avatar() {
     return (
         <MotionTransition position="bottom" className="bottom-0 right-0 hidden md:inline-block md:absolute ">
-            <Image src="/home-4.png" width="400" height="400" className="w-full h-full " alt="Particles " />
+            <Image src={home} width="400" height="400" className="w-full h-full " alt="Particles " />
         </MotionTransition>
     )
 }
--- components/circle-image.tsx
+++ components/circle-image.tsx
@@ -1,12 +1,12 @@
 "use client"
 
 import Image from 'next/image'
-import ServicesImage from "../public/services.png"
+import Services from "../public/services.png"
 
 const CircleImage = () => {
     return (
         <div className="bottom-0 right-0 hidden md:inline-block md:absolute">
-            <Image src={ServicesImage} width="300" height="300" className="w-full h-full " alt="Particles " />
+            <Image src={Services} width="300" height="300" className="w-full h-full " alt="Particles " />
         </div>
 
     );
